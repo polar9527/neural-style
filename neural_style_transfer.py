571f8e5cfe65ea137142673db273e52d63811a0e
polar9527@163.com <polar123456a>
2018-05-18 08:26:41 +0800

    deploy test
    Signed-off-by: polar9527@163.com <polar123456a>

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -301,3 +301,6 @@ for i in range(iterations):
     end_time = time.time()
     print('Image saved as', fname)
     print('Iteration %d completed in %ds' % (i, end_time - start_time))
+
+def test_deploy():
+    pass
